561178a6dfcf508b901184f30a343ed8f9b0f723
Liyize97126 <1216431704@qq.com>
2020-05-07 17:28:35 +0800

    0507-极光消息推送的集成

---
+++ app/src/main/java/com/bawei/dianshangjin16/MyApplication.java
@@ -0,0 +1,20 @@
+package com.bawei.dianshangjin16;
+
+import android.app.Application;
+
+import cn.jpush.android.api.JPushInterface;
+import cn.jpush.android.helper.Logger;
+
+/**
+ * 应用核心类
+ */
+public class MyApplication extends Application {
+    private static final String TAG = "JIGUANG-Push";
+    @Override
+    public void onCreate() {
+        Logger.d(TAG, "[ExampleApplication] onCreate");
+        super.onCreate();
+        JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
+        JPushInterface.init(this);     		// 初始化 JPush
+    }
+}
---
+++ app/src/main/java/com/bawei/dianshangjin16/PushService.java
@@ -0,0 +1,8 @@
+package com.bawei.dianshangjin16;
+
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+
+}
